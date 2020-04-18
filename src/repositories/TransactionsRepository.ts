c7d80de62f9cc6e8ef402ab8d26919a0d7571a82
Hugo Ribeiro <hgribeiro@live.com>
2020-04-17 22:56:10 -0300

    desafio pronto, passou no teste automatizado

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,56 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    // const transactionWithBalance = {
+    //   this.transactions,
+    //   this.transactions.getBalance(),
+    // };
+
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (acumulated: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            acumulated.income += transaction.value;
+            break;
+          case 'outcome':
+            acumulated.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
+        return acumulated;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+    // const somaIncome = this.transactions.reduce((acumulated, transaction) => {
+    //   if (transaction.type === 'income') {
+    //     return acumulated + transaction.value;
+    //   }
+    // }, 0);
+
+    // const balance = {
+    //   income: somaIncome,
+    //   outcome: somaOutcome,
+    //   total: somaIncome - somaOutcome,
+    // };
+    const total = income - outcome;
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
